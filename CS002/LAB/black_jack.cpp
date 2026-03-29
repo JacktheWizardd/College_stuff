0a7a7176ef37f531e24e926f9fd693927c61f060
Jack Truong <182292159+JacktheWizardd@users.noreply.github.com>
2026-03-28 19:56:00 -0700

    Add Rank enum and modify deck shuffle logic

--- CS002/LAB/black_jack.cpp
+++ CS002/LAB/black_jack.cpp
@@ -8,21 +8,44 @@
     int deck[deck_size];
 
     enum suits{club, diamond, heart, spade};
+    enum Rank {
+        ace,
+        one,
+        two,
+        three,
+        four,
+        five,
+        six,
+        seven,
+        eight,
+        nine,
+        ten,
+        eleven,
+        twelve,
+        thirdteen
+    };
 
     void Initialize_Deck(){
         for(int i = 0; i < deck_size; i++){
+            
+            //choosing deck[variable] then add number to it 
             deck[i] = i;
         }
     }
 
     void shuffle_deck(){
+        //random each time
         srand(time(0));
+
         for(int i = 51; i > 0; i--){
             int j = rand() % (i + 1);
+
+            //temporarity snapshot value from deck[i] variable
             int temp = deck[i];
+
+            //swap
             deck[i] = deck[j];
             deck[j] = temp;
-            cout << deck[i] << endl;
         }
     }
 
@@ -30,8 +53,8 @@
 
     int main(){
         Initialize_Deck();
-        cout << endl;
         shuffle_deck();
-
+        Rank rank = one;
+        cout << rank;
         return 0;
     }
